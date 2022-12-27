933c8b3e9be00e0605319019a73102ee4689a577
omatiz86 <omatiz86@gmail.com>
2022-12-27 18:55:31 -0500

    Creacion Carpetas y modularizar

---
+++ src/app/Core/shared/Components/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+
+import {HttpClientModule} from  "@angular/common/http";
+
+@NgModule({
+    imports: [
+        HttpClientModule
+    ],
+    declarations: [],
+    exports:[
+        HttpClientModule
+    ],
+    providers: []
+})
+
+export class SharedModule {
+    constructor () {}
+
+}
